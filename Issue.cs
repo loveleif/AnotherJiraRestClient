fcb04337f7f2f53315cd86fabaa98a22d609d3cb
Kristofer Leifland <peoplesbeer@gmail.com>
2012-06-14 16:40:49 +0200

    Added (almost) all fields to Issue.

--- Issue.cs
+++ Issue.cs
@@ -7,23 +7,228 @@ namespace AnotherJiraRestClient
 {
     /// <summary>
     /// Wrapper around the JSON object returned for a JIRA issue.
-    /// 
-    /// 
     /// </summary>
     public class Issue
     {
+        public string expand { get; set; }
+        public string id { get; set; }
+        public string self { get; set; }
+        public string key { get; set; }
+        public Fields fields { get; set; }
+    }
+
+    public class Fields
+    {
+        public Progress progress { get; set; }
+        public string summary { get; set; }
+        public Timetracking timetracking { get; set; }
+        public Issuetype issuetype { get; set; }
+        public Votes votes { get; set; }
+        public Resolution resolution { get; set; }
+        public List<object> fixVersions { get; set; }
+        public string resolutiondate { get; set; }
+        public int timespent { get; set; }
+        public Reporter reporter { get; set; }
+        public int aggregatetimeoriginalestimate { get; set; }
+        public string created { get; set; }
+        public string updated { get; set; }
+        public string description { get; set; }
+        public Priority priority { get; set; }
+        public string duedate { get; set; }
+        public List<object> issuelinks { get; set; }
+        public Watches watches { get; set; }
+        public Worklog worklog { get; set; }
+        public List<object> subtasks { get; set; }
+        public Status status { get; set; }
+        public List<string> labels { get; set; }
+        public int workratio { get; set; }
+        public Assignee assignee { get; set; }
+        public List<object> attachment { get; set; }
+        public int aggregatetimeestimate { get; set; }
+        public Project project { get; set; }
+        public List<object> versions { get; set; }
+        public string environment { get; set; }
+        public int timeestimate { get; set; }
+        public Aggregateprogress aggregateprogress { get; set; }
+        public List<object> components { get; set; }
+        public Comment comment { get; set; }
+        public int timeoriginalestimate { get; set; }
+        public int aggregatetimespent { get; set; }
+    }
+
+    public class Progress
+    {
+        public int progress { get; set; }
+        public int total { get; set; }
+        public int percent { get; set; }
+    }
+
+    public class Timetracking
+    {
+        public string originalEstimate { get; set; }
+        public string remainingEstimate { get; set; }
+        public string timeSpent { get; set; }
+        public int originalEstimateSeconds { get; set; }
+        public int remainingEstimateSeconds { get; set; }
+        public int timeSpentSeconds { get; set; }
+    }
+
+    public class Issuetype
+    {
+        public string self { get; set; }
+        public string id { get; set; }
+        public string description { get; set; }
+        public string iconUrl { get; set; }
+        public string name { get; set; }
+        public bool subtask { get; set; }
+    }
+
+    public class Votes
+    {
+        public string self { get; set; }
+        public int votes { get; set; }
+        public bool hasVoted { get; set; }
+    }
+
+    public class Resolution
+    {
+        public string self { get; set; }
+        public string id { get; set; }
+        public string description { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Reporter
+    {
+        public string self { get; set; }
+        public string name { get; set; }
+        public string emailAddress { get; set; }
+        public string displayName { get; set; }
+        public bool active { get; set; }
+    }
+
+    public class Priority
+    {
+        public string self { get; set; }
+        public string iconUrl { get; set; }
+        public string name { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Watches
+    {
+        public string self { get; set; }
+        public int watchCount { get; set; }
+        public bool isWatching { get; set; }
+    }
+
+    public class Author
+    {
+        public string self { get; set; }
+        public string name { get; set; }
+        public string emailAddress { get; set; }
+        public string displayName { get; set; }
+        public bool active { get; set; }
+    }
+
+    public class UpdateAuthor
+    {
+        public string self { get; set; }
+        public string name { get; set; }
+        public string emailAddress { get; set; }
+        public string displayName { get; set; }
+        public bool active { get; set; }
+    }
+
+    public class Worklog2
+    {
+        public string self { get; set; }
+        public Author author { get; set; }
+        public UpdateAuthor updateAuthor { get; set; }
+        public string comment { get; set; }
+        public string created { get; set; }
+        public string updated { get; set; }
+        public string started { get; set; }
+        public string timeSpent { get; set; }
+        public int timeSpentSeconds { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Worklog
+    {
+        public int startAt { get; set; }
+        public int maxResults { get; set; }
+        public int total { get; set; }
+        public List<Worklog2> worklogs { get; set; }
+    }
+
+    public class Status
+    {
+        public string self { get; set; }
+        public string description { get; set; }
+        public string iconUrl { get; set; }
+        public string name { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Assignee
+    {
+        public string self { get; set; }
+        public string name { get; set; }
+        public string emailAddress { get; set; }
+        public string displayName { get; set; }
+        public bool active { get; set; }
+    }
+
+    public class Project
+    {
+        public string self { get; set; }
         public string id { get; set; }
         public string key { get; set; }
+        public string name { get; set; }
+    }
 
-        public Fields fields { get; set; }
+    public class Aggregateprogress
+    {
+        public int progress { get; set; }
+        public int total { get; set; }
+        public int percent { get; set; }
+    }
 
-        public class Fields
-        {
-            public string summary { get; set; }
-            public string description { get; set; }
-            // TODO: Add more fields
-        }
+    public class Author2
+    {
+        public string self { get; set; }
+        public string name { get; set; }
+        public string emailAddress { get; set; }
+        public string displayName { get; set; }
+        public bool active { get; set; }
+    }
 
-        // TODO: Add more fields
+    public class UpdateAuthor2
+    {
+        public string self { get; set; }
+        public string name { get; set; }
+        public string emailAddress { get; set; }
+        public string displayName { get; set; }
+        public bool active { get; set; }
+    }
+
+    public class Comment2
+    {
+        public string self { get; set; }
+        public string id { get; set; }
+        public Author2 author { get; set; }
+        public string body { get; set; }
+        public UpdateAuthor2 updateAuthor { get; set; }
+        public string created { get; set; }
+        public string updated { get; set; }
+    }
+
+    public class Comment
+    {
+        public int startAt { get; set; }
+        public int maxResults { get; set; }
+        public int total { get; set; }
+        public List<Comment2> comments { get; set; }
     }
 }
