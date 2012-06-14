555865f093dc505fad5cfcef8ef55a27cdeac779
Kristofer Leifland <peoplesbeer@gmail.com>
2012-06-14 15:44:29 +0200

    Proof of concept for getting an issue.

---
+++ Issue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnotherJiraRestClient
+{
+    /// <summary>
+    /// Wrapper around the JSON object returned for a JIRA issue.
+    /// 
+    /// 
+    /// </summary>
+    public class Issue
+    {
+        public string id { get; set; }
+        public string key { get; set; }
+
+        public Fields fields { get; set; }
+
+        public class Fields
+        {
+            public string summary { get; set; }
+            public string description { get; set; }
+            // TODO: Add more fields
+        }
+
+        // TODO: Add more fields
+    }
+}
---
+++ JiraClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace AnotherJiraRestClient
+{
+    public class JiraClient
+    {
+        private readonly string baseUrl;
+        private readonly string userName;
+        private readonly string password;
+
+        public JiraClient(string baseUrl, string userName, string password)
+        {
+            this.baseUrl = baseUrl;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        /// <summary>
+        /// Execute a RestRequest using this JiraClient. See https://github.com/restsharp/RestSharp/wiki/Recommended-Usage.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient(baseUrl);
+            client.Authenticator = new HttpBasicAuthenticator(userName, password);
+            var response = client.Execute<T>(request);
+            return response.Data;
+        }
+
+        public Issue GetIssue(string issueKey)
+        {
+            var request = new RestRequest();
+            request.Resource = "/rest/api/2/issue/" + issueKey;
+            request.Method = Method.GET;
+            return Execute<Issue>(request);
+        }
+    }
+}
