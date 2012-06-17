b1e63d01f3a7470400b4ae0632457f87da907328
Kristofer Leifland <peoplesbeer@gmail.com>
2012-06-18 00:52:31 +0200

    Added comments.

--- JiraAccount.cs
+++ JiraAccount.cs
@@ -5,11 +5,25 @@ using System.Text;
 
 namespace AnotherJiraRestClient
 {
+    /// <summary>
+    /// Class representing a Jira server acount
+    /// </summary>
     [Serializable()]
     public class JiraAccount
     {
+        /// <summary>
+        /// Jira server url, for example https://example.atalassian.net
+        /// </summary>
         public string ServerUrl { get; set; }
+
+        /// <summary>
+        /// Jira server user name
+        /// </summary>
         public string User { get; set; }
+
+        /// <summary>
+        /// Jira server password
+        /// </summary>
         public string Password { get; set; }
     }
 }
